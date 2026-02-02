f1444843ce6b247fdcccc677585c739ed677a194
ahmadrashad647-dot <ahmadrashad647@gmail.com>
2026-02-03 01:23:45 +0530

    Create firebase-config.js

---
+++ js/firebase-config.js
@@ -0,0 +1,17 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyCrlP360Fpx-9tCyggJeoqLgkwnTlRTJ5o",
+  authDomain: "ncc-project-3b0f1.firebaseapp.com",
+  projectId: "ncc-project-3b0f1",
+  storageBucket: "ncc-project-3b0f1.firebasestorage.app",
+  messagingSenderId: "590441541895",
+  appId: "1:590441541895:web:2a861830fcb60f09f655a2"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
