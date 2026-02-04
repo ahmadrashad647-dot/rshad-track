cfb81150e2c80d26e9a16d315959b5867f25aa61
ahmed <ahmadrashad647@gmail.com>
2026-02-04 11:30:17 +0530

    Update view.js

--- js/view.js
+++ js/view.js
@@ -1,20 +1,36 @@
-db.collection("logs")
-.orderBy("time","desc")
-.onSnapshot(snapshot=>{
+firebase.auth().onAuthStateChanged(user=>{
 
-  const container = document.getElementById("entries");
-  container.innerHTML = "";
+  if(!user){
+    alert("Please login");
+    return;
+  }
 
-  snapshot.forEach(doc=>{
-    const d = doc.data();
-    container.innerHTML += `
-      <div class="list-item">
-        <div>
-          <b>${d.chapter}</b><br>
-          ${d.subject} - ${d.minutes} min
-        </div>
-      </div>
-    `;
-  });
+  db.collection("users")
+    .doc(user.uid)
+    .collection("logs")
+    .orderBy("time","desc")
+    .onSnapshot(snapshot=>{
+
+      const container = document.getElementById("entries");
+      container.innerHTML = "";
+
+      if(snapshot.empty){
+        container.innerHTML = "<p>No records found</p>";
+        return;
+      }
+
+      snapshot.forEach(doc=>{
+        const d = doc.data();
+        container.innerHTML += `
+          <div class="list-item">
+            <div>
+              <b>${d.chapter}</b><br>
+              ${d.subject} • ${d.minutes} min
+            </div>
+          </div>
+        `;
+      });
+
+    });
 
 });
